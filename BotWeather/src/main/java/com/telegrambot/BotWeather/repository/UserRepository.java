79bee351955530e00a402a0d3c352ce6388fc248
Korshunov Mihail <71220872+MihailKoshunov@users.noreply.github.com>
2022-10-30 21:37:53 +0300

    Delete BotWeather directory

--- BotWeather/src/main/java/com/telegrambot/BotWeather/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.telegrambot.BotWeather.repository;
-
-import com.telegrambot.BotWeather.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User,String> {
-}
